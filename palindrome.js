fadfee789ecc080e95c14fcdb56a9806f76a9c4c
paibhav112 <vaibhavpandey876@gmai.com>
2023-01-03 23:51:09 +0530

    Wrong palindrome

---
+++ palindrome.js
@@ -0,0 +1,12 @@
+let a=
+  let bag="";
+ for ( let i=N-1;i>=0;i-- ) {
+     bag+=str[i];
+ }
+        if (str==bag) {
+            console.log("Yes");
+        }
+    else{console.log("No");
+    }
+    
+ }  
