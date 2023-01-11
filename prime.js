7f4c6f338b903c557d0ccfff461a9da3645d28ed
paibhav112 <vaibhavpandey876@gmai.com>
2023-01-12 00:04:00 +0530

    Check the file

---
+++ prime.js
@@ -0,0 +1,16 @@
+      let count=0
+      let num=5
+     for(let i=0;i<num;i++){
+	if(num%i==0){
+	   count++
+	}
+    }
+     if(count==2){
+	console.log("Yes")
+       }
+      else{
+        console.log("No")
+
+
+
+
